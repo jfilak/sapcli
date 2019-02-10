3bc98362d9d22a6c2ba41875bdd6b1cbbcce9a01
Jakub Filak <jakub@thefilaks.net>
2019-02-10 20:39:06 +0100

    test: connection returns 200 by default
    There is a debug log printing out status_code, therefore the mock connection
    must never return None.

--- test/unit/mock.py
+++ test/unit/mock.py
@@ -17,19 +17,18 @@ class Request(NamedTuple):
     params: Dict
 
 
+def ok_responses():
+
+    yield Response(text='', status_code=200, headers={})
+
+
 class Connection:
 
     def __init__(self, responses=None):
         self.execs = list()
-
-        self._resp_iter = None
-        if responses is not None:
-            self._resp_iter = iter(responses)
+        self._resp_iter = ok_responses() if responses is None else iter(responses)
 
     def execute(self, method, adt_uri, params=None, headers=None, body=None):
         self.execs.append(Request(method, adt_uri, headers, body, params))
 
-        if self._resp_iter is not None:
-            return next(self._resp_iter)
-
-        return None
+        return next(self._resp_iter)
