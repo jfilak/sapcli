0d32a08772930a0b070501f5d594eea0fad15d2e
Jakub Filak <jakub@thefilaks.net>
2019-03-07 21:44:14 +0100

    tests: create infrastructure for adding new class includes

--- test/unit/test_sap_adt_class.py
+++ test/unit/test_sap_adt_class.py
@@ -72,17 +72,17 @@ class TestADTClass(unittest.TestCase):
         self.maxDiff = None
         self.assertEqual(put_request.body, FIXTURE_CLASS_MAIN_CODE)
 
-    def test_adt_class_read_tests(self):
-        conn = Connection([TEST_CLASSES_READ_RESPONSE_OK])
+    def include_read_test(self, response, getter, includes_uri):
+        conn = Connection([response])
 
         clas = sap.adt.Class(conn, 'ZCL_HELLO_WORLD')
-        source_code = clas.test_classes.text
+        source_code = getter(clas).text
 
-        self.assertEqual(source_code, TEST_CLASSES_READ_RESPONSE_OK.text)
+        self.assertEqual(source_code, response.text)
 
         self.assertEqual(
             [(e.method, e.adt_uri) for e in conn.execs],
-            [('GET', '/sap/bc/adt/oo/classes/zcl_hello_world/includes/testclasses')])
+            [('GET', f'/sap/bc/adt/oo/classes/zcl_hello_world/{includes_uri}')])
 
         get_request = conn.execs[0]
         self.assertEqual(sorted(get_request.headers), ['Accept'])
@@ -90,17 +90,20 @@ class TestADTClass(unittest.TestCase):
 
         self.assertIsNone(get_request.params)
 
-    def test_adt_class_write_tests(self):
+    def test_adt_class_read_tests(self):
+        self.include_read_test(TEST_CLASSES_READ_RESPONSE_OK, lambda clas: clas.test_classes, 'includes/testclasses')
+
+    def include_write_test(self, getter, includes_uri):
         conn = Connection([LOCK_RESPONSE_OK, EMPTY_RESPONSE_OK])
 
         clas = sap.adt.Class(conn, 'ZCL_HELLO_WORLD')
         clas.lock()
 
-        clas.test_classes.change_text('* new test classes')
+        getter(clas).change_text('* new content')
 
         self.assertEqual(
             [(e.method, e.adt_uri) for e in conn.execs[1:] ],
-            [('PUT', '/sap/bc/adt/oo/classes/zcl_hello_world/includes/testclasses')])
+            [('PUT', f'/sap/bc/adt/oo/classes/zcl_hello_world/{includes_uri}')])
 
         put_request = conn.execs[1]
         self.assertEqual(sorted(put_request.headers), ['Accept', 'Content-Type'])
@@ -111,7 +114,10 @@ class TestADTClass(unittest.TestCase):
         self.assertEqual(put_request.params['lockHandle'], 'win')
 
         self.maxDiff = None
-        self.assertEqual(put_request.body, '* new test classes')
+        self.assertEqual(put_request.body, '* new content')
+
+    def test_adt_class_write_tests(self):
+        self.include_write_test(lambda clas: clas.test_classes, 'includes/testclasses')
 
 
 if __name__ == '__main__':
