f42f3b300505d09e0a86792a3a1faa506d7a6704
Jakub Filak <jakub@thefilaks.net>
2020-02-18 22:58:35 +0100

    tests: add a base class for console output testing

--- test/unit/mock.py
+++ test/unit/mock.py
@@ -2,6 +2,8 @@ import copy
 from typing import Dict, NamedTuple
 from io import StringIO
 from argparse import ArgumentParser
+
+import unittest
 from unittest.mock import patch
 
 import sap.adt
@@ -184,3 +186,18 @@ class PatcherTestCase:
 
         for patcher in self._patchers.values():
             patcher.__exit__(None, None, None)
+
+
+class ConsoleOutputTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.console = BufferConsole()
+
+    def assertEmptyConsole(self, console,):
+        self.assertEqual(console.capout, '')
+        self.assertEqual(console.caperr, '')
+
+    def assertConsoleContents(self, console, stdout='', stderr=''):
+        self.assertEqual(console.capout, stdout)
+        self.assertEqual(console.caperr, stderr)
+
