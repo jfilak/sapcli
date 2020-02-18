2e6f2bbc55987da77a9c7b2782a62e62a1239271
Jakub Filak <jakub@thefilaks.net>
2020-02-18 22:18:38 +0100

    cli: checkin config loader tested

---
+++ test/unit/fixtures_abap.py
@@ -0,0 +1,17 @@
+ABAP_GIT_DEFAULT_XML='''<?xml version="1.0" encoding="utf-8"?>
+<asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
+ <asx:values>
+  <DATA>
+   <MASTER_LANGUAGE>E</MASTER_LANGUAGE>
+   <STARTING_FOLDER>/src/</STARTING_FOLDER>
+   <FOLDER_LOGIC>PREFIX</FOLDER_LOGIC>
+   <IGNORE>
+    <item>/.travis.yml</item>
+    <item>/CONTRIBUTING.md</item>
+    <item>/LICENSE</item>
+    <item>/README.md</item>
+   </IGNORE>
+  </DATA>
+ </asx:values>
+</asx:abap>
+'''
