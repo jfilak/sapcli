6b787fc490c60c5b3d38822613da8fdb7004f967
Jakub Filak <jakub@thefilaks.net>
2020-07-26 18:12:09 +0200

    rfc: add wrappers for creating sers

---
+++ sap/rfc/user.py
@@ -0,0 +1,275 @@
+"""User management over RFC"""
+
+import datetime
+
+from typing import Dict, Union, List
+
+from sap.rfc.core import RFCParams
+from sap.rfc.bapi import BAPIError
+
+
+UserId = str
+UserAddress = Dict[str, str]
+
+
+def add_to_dict_if_not_none(target_dict, target_key, value):
+    """Adds the given value to the give dict as the given key
+       only if the given value is not None.
+    """
+
+    if value is None:
+        return False
+
+    target_dict[target_key] = value
+    return True
+
+
+def add_to_dict_if_not_present(target_dict, target_key, value):
+    """Adds the given value to the give dict as the given key
+       only if the given key is not in the given dict yet.
+    """
+
+    if target_key in target_dict:
+        return False
+
+    target_dict[target_key] = value
+    return True
+
+
+def sap_date_from(the_date: datetime.date) -> str:
+    """Converts Python date to string"""
+
+    return the_date.strftime('%Y%m%d')
+
+
+def today_sap_date() -> str:
+    """Returns SAP date string for today"""
+
+    return sap_date_from(datetime.date.today())
+
+
+class UserBuilder:
+    """An utility class for building SAP user parameters"""
+
+    def __init__(self):
+        self._username = None
+        self._address = None
+        self._logondata = None
+        self._password = None
+        self._alias = None
+
+    @property
+    def _address_data(self) -> UserAddress:
+        if self._address is None:
+            self._address = {}
+
+        return self._address
+
+    @property
+    def _password_data(self) -> Dict[str, str]:
+        if self._password is None:
+            self._password = {}
+
+        return self._password
+
+    @property
+    def _alias_data(self) -> Dict[str, str]:
+        if self._alias is None:
+            self._alias = {}
+
+        return self._alias
+
+    @property
+    def _logondata_data(self) -> Dict[str, str]:
+        if self._logondata is None:
+            self._logondata = {}
+
+        return self._logondata
+
+    def set_username(self, username: str):
+        """Sets user name for logon"""
+
+        self._username = username
+        return self
+
+    def set_first_name(self, first_name: str):
+        """Sets user's first name"""
+
+        self._address_data['FIRSTNAME'] = first_name
+        return self
+
+    def set_last_name(self, last_name: str):
+        """Sets user's last name"""
+
+        self._address_data['LASTNAME'] = last_name
+        return self
+
+    def set_email_address(self, email_address: str):
+        """Sets user's email address"""
+
+        self._address['E_MAIL'] = email_address
+        return self
+
+    def set_password(self, password: str):
+        """Sets user's password - works only for the user type Service"""
+
+        self._password_data['BAPIPWD'] = password
+        return self
+
+    def set_alias(self, alias: str):
+        """Sets user's alias for HTTP authentication"""
+
+        self._alias_data['USERALIAS'] = alias
+        return self
+
+    def set_type(self, typ: str):
+        """Sets user's type"""
+
+        self._logondata_data['USTYP'] = typ
+        return self
+
+    def set_valid_from(self, start_date: Union[str]):
+        """Sets user's start validity date"""
+
+        if isinstance(start_date, str):
+            self._logondata_data['GLTGV'] = start_date
+        else:
+            raise ValueError()
+
+        return self
+
+    def set_valid_to(self, end_date: Union[str]):
+        """Sets user's end validity date"""
+
+        if isinstance(end_date, str):
+            self._logondata_data['GLTGB'] = end_date
+        else:
+            raise ValueError()
+
+        return self
+
+    def build_rfc_params(self) -> RFCParams:
+        """Creates RFC parameters"""
+
+        params = dict()
+
+        add_to_dict_if_not_none(params, 'USERNAME', self._username)
+        add_to_dict_if_not_none(params, 'ADDRESS', self._address)
+        add_to_dict_if_not_none(params, 'PASSWORD', self._password)
+        add_to_dict_if_not_none(params, 'ALIAS', self._alias)
+
+        add_to_dict_if_not_present(self._logondata_data, 'GLTGV', today_sap_date())
+        add_to_dict_if_not_present(self._logondata_data, 'GLTGB', '20991231')
+
+        add_to_dict_if_not_none(params, 'LOGONDATA', self._logondata)
+
+        return params
+
+
+class UserRoleAssignmentBuilder:
+    """An utility class for building SAP user roles assignment parameters"""
+
+    def __init__(self, user):
+        self._user = user
+        self._roles = list()
+
+    def add_roles(self, role_names: List[str]):
+        """Set assigned roles name"""
+
+        self._roles = role_names
+        return self
+
+    def build_rfc_params(self) -> RFCParams:
+        """Creates RFC parameters"""
+
+        if not self._roles:
+            return None
+
+        rfc_table = []
+
+        for role_name in self._roles:
+            table_row = {'AGR_NAME': role_name}
+
+            add_to_dict_if_not_present(table_row, 'FROM_DAT', today_sap_date())
+            add_to_dict_if_not_present(table_row, 'TO_DAT', '20991231')
+
+            rfc_table.append(table_row)
+
+        return {
+            'USERNAME': self._user,
+            'ACTIVITYGROUPS': rfc_table
+        }
+
+
+class UserProfileAssignmentBuilder:
+    """An utility class for building SAP user profiles assignment parameters"""
+
+    def __init__(self, user):
+        self._user = user
+        self._profiles = list()
+
+    def add_profiles(self, profile_names: List[str]):
+        """Set assigned profiles name"""
+
+        self._profiles = profile_names
+        return self
+
+    def build_rfc_params(self) -> RFCParams:
+        """Creates RFC parameters"""
+
+        if not self._profiles:
+            return None
+
+        rfc_table = []
+
+        for profile_name in self._profiles:
+            table_row = {'BAPIPROF': profile_name}
+            rfc_table.append(table_row)
+
+        return {
+            'USERNAME': self._user,
+            'PROFILES': rfc_table
+        }
+
+
+class UserManager:
+    """Proxy to SAP API managing Users"""
+
+    # pylint: disable=no-self-use
+    def _call_bapi_method(self, connection, method_name, kwargs):
+        resp = connection.call(method_name, **kwargs)
+        BAPIError.raise_for_error(resp['RETURN'], resp)
+        return resp
+
+    # pylint: disable=no-self-use
+    def user_builder(self) -> UserBuilder:
+        """Returns instance of User Parameters builder"""
+
+        return UserBuilder()
+
+    def create_user(self, connection, user_builder: UserBuilder) -> UserId:
+        """Creates a new user for the given user data"""
+
+        self._call_bapi_method(connection, 'BAPI_USER_CREATE1', user_builder.build_rfc_params())
+
+    # pylint: disable=no-self-use
+    def user_role_assignment_builder(self, username: str) -> UserRoleAssignmentBuilder:
+        """Returns instance of User Role Assignment builder"""
+
+        return UserRoleAssignmentBuilder(username)
+
+    def assign_roles(self, connection, roles_builder: UserRoleAssignmentBuilder) -> None:
+        """Assigns roles"""
+
+        self._call_bapi_method(connection, 'BAPI_USER_ACTGROUPS_ASSIGN', roles_builder.build_rfc_params())
+
+    # pylint: disable=no-self-use
+    def user_profile_assignment_builder(self, username: str) -> UserProfileAssignmentBuilder:
+        """Returns instance of User Profile Assignment builder"""
+
+        return UserProfileAssignmentBuilder(username)
+
+    def assign_profiles(self, connection, profiles_builder: UserProfileAssignmentBuilder) -> None:
+        """Assigns profiles"""
+
+        self._call_bapi_method(connection, 'BAPI_USER_PROFILES_ASSIGN', profiles_builder.build_rfc_params())
---
+++ test/unit/test_sap_rfc_user.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+
+import datetime
+
+import unittest
+from unittest.mock import Mock
+
+from sap.rfc.bapi import BAPIError
+from sap.rfc.user import add_to_dict_if_not_none, add_to_dict_if_not_present, today_sap_date, \
+         UserBuilder, UserRoleAssignmentBuilder, UserProfileAssignmentBuilder, UserManager
+
+from test_sap_rfc_bapi import create_bapiret_error
+
+
+class SAPRFCUserAux(unittest.TestCase):
+
+    def test_add_to_dict_if_not_none_none(self):
+        target = dict()
+
+        ret = add_to_dict_if_not_none(target, 'key', None)
+
+        self.assertNotIn('key', target)
+        self.assertFalse(ret)
+
+    def test_add_to_dict_if_not_none_not_none(self):
+        target = dict()
+
+        ret = add_to_dict_if_not_none(target, 'key', 'value')
+
+        self.assertIn('key', target)
+        self.assertEqual(target, {'key': 'value'})
+        self.assertTrue(ret)
+
+    def test_add_to_dict_if_not_present_yes(self):
+        target = dict()
+
+        ret = add_to_dict_if_not_present(target, 'key', 'value')
+
+        self.assertIn('key', target)
+        self.assertEqual(target, {'key': 'value'})
+        self.assertTrue(ret)
+
+    def test_add_to_dict_if_not_present_no(self):
+        target = {'key': 'value'}
+
+        ret = add_to_dict_if_not_present(target, 'key', 'foo')
+
+        self.assertIn('key', target)
+        self.assertEqual(target, {'key': 'value'})
+        self.assertFalse(ret)
+
+class TestUserBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.builder = UserBuilder()
+
+    def test_no_parameters_provided(self):
+        params = self.builder.build_rfc_params()
+
+        date = datetime.date.today().strftime('%Y%m%d')
+
+        self.assertEqual(params, {'LOGONDATA': {'GLTGV': date, 'GLTGB': '20991231'}})
+
+    def test_valid_from_value_error(self):
+        with self.assertRaises(ValueError):
+            self.builder.set_valid_from(datetime.date.today())
+
+    def test_valid_to_value_error(self):
+        with self.assertRaises(ValueError):
+            self.builder.set_valid_to(datetime.date.today())
+
+    def test_all_parameters_provided(self):
+        username = 'FOO'
+        first_name = 'First'
+        last_name = 'Last'
+        email_address = 'email@example.org'
+        password = 'Password'
+        alias = 'HTTP_ALIAS'
+        typ = 'S'
+        start_date = '20200101'
+        end_date = '20201231'
+
+        self.assertEqual(self.builder, self.builder.set_username(username))
+        self.assertEqual(self.builder, self.builder.set_first_name(first_name))
+        self.assertEqual(self.builder, self.builder.set_last_name(last_name))
+        self.assertEqual(self.builder, self.builder.set_email_address(email_address))
+        self.assertEqual(self.builder, self.builder.set_password(password))
+        self.assertEqual(self.builder, self.builder.set_alias(alias))
+        self.assertEqual(self.builder, self.builder.set_type(typ))
+        self.assertEqual(self.builder, self.builder.set_valid_from(start_date))
+        self.assertEqual(self.builder, self.builder.set_valid_to(end_date))
+
+        params = self.builder.build_rfc_params()
+
+        self.assertEqual(params, {
+            'USERNAME': username,
+            'ALIAS': {
+                'USERALIAS': alias
+            },
+            'ADDRESS': {
+                'FIRSTNAME': first_name,
+                'LASTNAME': last_name,
+                'E_MAIL': email_address
+            },
+            'PASSWORD': {
+                'BAPIPWD': password
+            },
+            'LOGONDATA': {
+                'USTYP': typ,
+                'GLTGV': start_date,
+                'GLTGB': end_date
+            }
+        })
+
+
+class TestUserRoleAssignmentBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.username = 'LOGON'
+        self.builder = UserRoleAssignmentBuilder(self.username)
+
+    def test_no_parameters_provided(self):
+        params = self.builder.build_rfc_params()
+        self.assertIsNone(params)
+
+    def test_all_parameters_provided(self):
+        self.assertEqual(self.builder, self.builder.add_roles(['1', '2', '3']))
+
+        params = self.builder.build_rfc_params()
+
+        start_date = today_sap_date()
+        self.maxDiff = None
+        self.assertEqual(params, {
+            'USERNAME': self.username,
+            'ACTIVITYGROUPS': [
+                {'AGR_NAME': '1',
+                 'FROM_DAT': start_date,
+                 'TO_DAT': '20991231'
+                },
+                {'AGR_NAME': '2',
+                 'FROM_DAT': start_date,
+                 'TO_DAT': '20991231'
+                },
+                {'AGR_NAME': '3',
+                 'FROM_DAT': start_date,
+                 'TO_DAT': '20991231'
+                }
+            ]
+        })
+
+
+class TestUserProfileAssignmentBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.username = 'LOGON'
+        self.builder = UserProfileAssignmentBuilder(self.username)
+
+    def test_no_parameters_provided(self):
+        params = self.builder.build_rfc_params()
+        self.assertIsNone(params)
+
+    def test_all_parameters_provided(self):
+        self.assertEqual(self.builder, self.builder.add_profiles(['1', '2', '3']))
+
+        params = self.builder.build_rfc_params()
+
+        start_date = today_sap_date()
+        self.maxDiff = None
+        self.assertEqual(params, {
+            'USERNAME': self.username,
+            'PROFILES': [
+                {'BAPIPROF': '1',
+                },
+                {'BAPIPROF': '2',
+                },
+                {'BAPIPROF': '3',
+                }
+            ]
+        })
+
+
+class TestUserManager(unittest.TestCase):
+
+    def setUp(self):
+        self.bapirettab = []
+        self.response = {'RETURN': self.bapirettab}
+
+        self.connection = Mock()
+        self.connection.call = Mock()
+        self.connection.call.return_value = self.response
+
+        self.username = 'logon'
+        self.manager = UserManager()
+
+    def test_user_builder(self):
+        self.assertIsNotNone(self.manager.user_builder())
+
+    def test_user_role_assignment_builder(self):
+        builder = self.manager.user_role_assignment_builder(self.username)
+        self.assertIsNotNone(builder)
+        self.assertEqual(builder._user, self.username)
+
+    def test_user_profile_assignment_builder(self):
+        builder = self.manager.user_profile_assignment_builder(self.username)
+        self.assertIsNotNone(builder)
+        self.assertEqual(builder._user, self.username)
+
+    def test_create_user_no_error(self):
+        user_builder = self.manager.user_builder()
+        user_builder.set_username(self.username)
+
+        self.manager.create_user(self.connection, user_builder)
+
+        self.connection.call.assert_called_once_with('BAPI_USER_CREATE1', **user_builder.build_rfc_params())
+
+    def test_create_user_with_error(self):
+        user_builder = self.manager.user_builder()
+        user_builder.set_username(self.username)
+
+        self.bapirettab.append(create_bapiret_error('Error message'))
+
+        with self.assertRaises(BAPIError) as caught:
+            self.manager.create_user(self.connection, user_builder)
+
+        self.connection.call.assert_called_once()
+
+        self.assertEqual(str(caught.exception), 'E Error message')
+        self.assertEqual(caught.exception.response, self.response)
+
+    def test_assign_roles_no_error(self):
+        builder = self.manager.user_role_assignment_builder(self.username)
+
+        builder.add_roles(['foo'])
+
+        self.manager.assign_roles(self.connection, builder)
+
+        self.connection.call.assert_called_once_with('BAPI_USER_ACTGROUPS_ASSIGN', **builder.build_rfc_params())
+
+    def test_assign_profiles_no_error(self):
+        builder = self.manager.user_profile_assignment_builder(self.username)
+
+        builder.add_profiles(['foo'])
+
+        self.manager.assign_profiles(self.connection, builder)
+
+        self.connection.call.assert_called_once_with('BAPI_USER_PROFILES_ASSIGN', **builder.build_rfc_params())
