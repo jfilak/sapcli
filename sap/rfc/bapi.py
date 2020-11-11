bdedc33abc408d868f7cd870ed1d397e04e1c577
Jakub Filak <jakub@thefilaks.net>
2020-11-11 23:26:31 +0100

    rfc: add BAPI return value helpers

--- sap/rfc/bapi.py
+++ sap/rfc/bapi.py
@@ -6,31 +6,84 @@ from sap.errors import SAPCliError
 from sap.rfc.core import RFCResponse
 
 
-class BAPIError(SAPCliError):
-    """RFC BAPI error"""
+BAPIReturnRFC = Union[RFCResponse, List[RFCResponse]]
 
-    def __init__(self, bapirettab, response):
-        super().__init__(
-            '\n'.join(
-                ['/'.join([bapiret['TYPE'], bapiret['ID'], bapiret['NUMBER'] + ': ' + bapiret['MESSAGE']])
-                 for bapiret in bapirettab]))
 
-        self.bapirettab = bapirettab
-        self.response = response
+def bapi_message_to_str(bapiret: RFCResponse):
+    """Converts a single BAPI return value to a human readable string"""
+
+    return '/'.join([bapiret['TYPE'], bapiret['ID'], bapiret['NUMBER'] + ': ' + bapiret['MESSAGE']])
+
+
+class BAPIReturn:
+    """BAPI return value"""
+
+    def __init__(self, bapi_return: BAPIReturnRFC):
+        if isinstance(bapi_return, dict):
+            self._bapirettab = [bapi_return]
+        elif isinstance(bapi_return, list):
+            self._bapirettab = bapi_return
+        else:
+            raise ValueError(f'Neither dict nor list BAPI return type: {type(bapi_return).__name__}')
+
+        for bapiret in self._bapirettab:
+            if bapiret['TYPE'] == 'E':
+                self._error_message = bapi_message_to_str(bapiret)
+                break
+        else:
+            self._error_message = None
+
+    def __str__(self):
+        return '\n'.join([bapi_message_to_str(bapiret) for bapiret in self._bapirettab])
+
+    def __getitem__(self, index):
+        return self._bapirettab[index]
 
     def contains(self, msg_class, msg_number):
         """"Returns True if the list of messages contains specified Message
             Class and Message Number.
         """
 
-        return any((msg['ID'] == msg_class and msg['NUMBER'] == msg_number for msg in self.bapirettab))
+        return any((msg['ID'] == msg_class and msg['NUMBER'] == msg_number for msg in self._bapirettab))
+
+    @property
+    def is_error(self):
+        """Returns True if the BAPI response represents an error.
+           Otherwise returns False.
+        """
+
+        return self._error_message is not None
+
+    @property
+    def is_empty(self):
+        """Returns True for BAPI response without any message."""
+
+        return len(self._bapirettab) == 0
+
+    @property
+    def error_message(self):
+        """Returns the error message if there is any. If the BAPI return value
+           does not represent an error, the property holds None.
+        """
+
+        return self._error_message
+
+
+class BAPIError(SAPCliError):
+    """RFC BAPI error"""
+
+    def __init__(self, bapireturn: BAPIReturn, response):
+        super().__init__(str(bapireturn))
+
+        self.bapiret = bapireturn
+        self.response = response
 
     @staticmethod
-    def raise_for_error(bapiret: Union[RFCResponse, List[RFCResponse]], response) -> None:
+    def raise_for_error(bapiret: BAPIReturnRFC, response) -> BAPIReturn:
         """If the given BAPI response contains error raise an exception"""
 
-        if not isinstance(bapiret, list):
-            bapiret = [bapiret]
+        bapi_return = BAPIReturn(bapiret)
+        if bapi_return.is_error:
+            raise BAPIError(bapi_return, response)
 
-        if bapiret and any((bapiret['TYPE'] == 'E' for bapiret in bapiret)):
-            raise BAPIError(bapiret, response)
+        return bapi_return
--- test/unit/test_sap_rfc_bapi.py
+++ test/unit/test_sap_rfc_bapi.py
@@ -3,7 +3,10 @@
 import unittest
 from unittest.mock import Mock
 
-from sap.rfc.bapi import BAPIError
+from sap.rfc.bapi import (
+    BAPIReturn,
+    BAPIError
+)
 
 
 def create_bapiret(typ:str=None, message:str=None, msg_class:str=None, msg_number:str=None):
@@ -18,7 +21,7 @@ def create_bapiret_warning(message:str):
     return create_bapiret(typ='W', message=message, msg_class='WRN', msg_number='777')
 
 
-class TestBAPIError(unittest.TestCase):
+class TestBAPIReturn(unittest.TestCase):
 
     def setUp(self):
         self.message_e = 'Error message'
@@ -27,15 +30,50 @@ class TestBAPIError(unittest.TestCase):
         self.bapirettab = [create_bapiret_error(self.message_e), create_bapiret_warning(self.message_w)]
         self.response = Mock()
 
+    def test_ctor_value_error(self):
+        with self.assertRaises(ValueError) as caught:
+            BAPIReturn("wrong type")
+
+        self.assertEqual(str(caught.exception), "Neither dict nor list BAPI return type: str")
+
+    def test_is_empty(self):
+        self.assertTrue(BAPIReturn([]).is_empty)
+
+    def test_not_is_empty(self):
+        self.assertFalse(BAPIReturn(self.bapirettab).is_empty)
+
+    def test_is_error_no_error(self):
+        self.assertFalse(BAPIReturn(self.bapirettab[1]).is_error)
+
+    def test_is_error_with_error(self):
+        self.assertTrue(BAPIReturn(self.bapirettab).is_error)
+
+    def test_error_message_no_error(self):
+        self.assertIsNone(BAPIReturn(self.bapirettab[1]).error_message)
+
+    def test_error_message_with_error(self):
+        self.assertEqual(BAPIReturn(self.bapirettab).error_message, 'E/ERR/333: Error message')
+
+
+class TestBAPIError(unittest.TestCase):
+
+    def setUp(self):
+        self.message_e = 'Error message'
+        self.message_w = 'Warning message'
+
+        self.bapirettab = [create_bapiret_error(self.message_e), create_bapiret_warning(self.message_w)]
+        self.bapiret = BAPIReturn(self.bapirettab)
+        self.response = Mock()
+
     def assertExDataMatch(self, ex):
         self.assertEqual(str(ex), '''E/ERR/333: Error message
 W/WRN/777: Warning message''')
 
-        self.assertEqual(ex.bapirettab, self.bapirettab)
+        self.assertEqual(ex.bapiret._bapirettab, self.bapirettab)
         self.assertEqual(ex.response, self.response)
 
     def test_ctor_join_list(self):
-        ex = BAPIError(self.bapirettab, self.response)
+        ex = BAPIError(self.bapiret, self.response)
         self.assertExDataMatch(ex)
 
     def test_raises_for_error(self):
@@ -48,12 +86,12 @@ W/WRN/777: Warning message''')
         with self.assertRaises(BAPIError) as caught:
             BAPIError.raise_for_error(self.bapirettab[0], self.response)
 
-        self.assertEqual(caught.exception.bapirettab, self.bapirettab[0:1])
+        self.assertEqual(caught.exception.bapiret._bapirettab, self.bapiret[0:1])
 
     def test_does_not_raise(self):
         BAPIError.raise_for_error([create_bapiret_warning(self.message_w)], self.response)
 
     def test_contains(self):
-        ex = BAPIError(self.bapirettab, self.response)
-        self.assertTrue(ex.contains('ERR', '333'))
-        self.assertFalse(ex.contains('NOP', '222'))
+        ex = BAPIError(self.bapiret, self.response)
+        self.assertTrue(ex.bapiret.contains('ERR', '333'))
+        self.assertFalse(ex.bapiret.contains('NOP', '222'))
