d9fcdebf766f7c3cd68cce698c8b7c84f1da1e7f
Alexander Kreuzer <alexander.kreuzer@sap.com>
2021-12-30 06:55:06 -0800

    Minor fixes
    Improved typing, fixed socket issue with macos in sap/rest/connection.py

--- test/unit/test_sap_cli_object.py
+++ test/unit/test_sap_cli_object.py
@@ -8,7 +8,7 @@ from types import SimpleNamespace
 
 import sap.cli.object
 
-from mock import Connection, Response, patch_get_print_console_with_buffer
+from mock import patch_get_print_console_with_buffer
 from fixtures_adt import DummyADTObject, LOCK_RESPONSE_OK, EMPTY_RESPONSE_OK, OBJECT_METADATA
 from fixtures_adt_wb import MessageBuilder
 
