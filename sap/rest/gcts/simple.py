5b63205456cc1fd45926467dc55a1e0b0ffcee16
Jakub Filak <jakub@thefilaks.net>
2022-09-21 11:57:40 +0200

    gcts: fix crash of listing user credentials with no credentials
    I am not sure if it's easily reproducible but I had a user without
    any credentials configured and sapcli crashed.

--- sap/rest/gcts/simple.py
+++ sap/rest/gcts/simple.py
@@ -103,7 +103,15 @@ def get_user_credentials(connection):
     """Get Token for the currently logged in user"""
 
     response = connection.get_json('user')
-    user_credentials = [cred for cred in response['user']['config'] if cred['key'] == 'USER_AUTH_CRED_ENDPOINTS']
+    user_data = response.get('user', None)
+    if user_data is None:
+        raise SAPCliError('gCTS response does not contain \'user\'')
+
+    config_data = user_data.get('config', None)
+    if config_data is None:
+        return {}
+
+    user_credentials = [cred for cred in config_data if cred['key'] == 'USER_AUTH_CRED_ENDPOINTS']
     return json.loads(user_credentials[0]['value'])
 
 
