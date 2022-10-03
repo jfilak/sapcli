d37df0a7a136b4f22f4f633b668a4f4bb33c7737
palubaj <juraj.paluba@sap.com>
2022-10-03 18:41:40 +0200

    rest: parse HTML server errors
    --- v2
    Incorrporated review comments: More general parsing of HTML and improved
    tests.

--- test/unit/test_sap_rest_errors.py
+++ test/unit/test_sap_rest_errors.py
@@ -3,9 +3,16 @@
 import unittest
 from unittest.mock import Mock
 
+from fixtures_sap_rest_error import (
+    GCTS_RESPONSE_FORBIDDEN,
+    GCTS_RESPONSE_FORBIDDEN_NO_ERROR_HEADER,
+    GCTS_RESPONSE_FORBIDDEN_NO_ERROR_MESSAGE,
+)
+
 from sap.rest.errors import (
     UnauthorizedError,
     TimedOutRequestError,
+    HTTPRequestError,
 )
 
 
@@ -35,3 +42,29 @@ class TestTimeoutError(unittest.TestCase):
         inst = TimedOutRequestError(request, timeout)
 
         self.assertEqual(str(inst), f'The request {request.method} {request.url} took more than {timeout}s')
+
+
+class TestHttpRequestError(unittest.TestCase):
+
+    def test_str_and_repr_with_match(self):
+        response = GCTS_RESPONSE_FORBIDDEN
+
+        inst = HTTPRequestError('Request', response)
+
+        self.assertEqual(str(inst), '403 Forbidden\n'
+                                    'The request has been blocked by UCON.\n'
+                                    'And the multiline error message.')
+
+    def test_str_and_repr_no_error_header(self):
+        response = GCTS_RESPONSE_FORBIDDEN_NO_ERROR_HEADER
+
+        inst = HTTPRequestError('Request', response)
+
+        self.assertEqual(str(inst), f'{response.status_code}\n{response.text}')
+
+    def test_str_and_repr_no_error_message(self):
+        response = GCTS_RESPONSE_FORBIDDEN_NO_ERROR_MESSAGE
+
+        inst = HTTPRequestError('Request', response)
+
+        self.assertEqual(str(inst), f'{response.status_code}\n{response.text}')
