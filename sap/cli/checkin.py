839e95ad6b972ad2e5b2f3da5f9219f30749bcd6
palubaj <juraj.paluba@sap.com>
2023-06-13 17:02:26 +0200

    checkin: format the abapgit source file
    Function modules can be in abapgit format, meaning that the parameters
    are specified in special comment block. I have create a function which
    parses parameters from this comment block and writes them in
    ADT-readable format.
    
    Note, for TABLES parameters the keyword `STRUCTURE` is used instead of
    `TYPE`. I have tested it in SAP GUI. Assigning typing `TYPE` for a
    TABLES parameter is represented as `STRUCTURE` in source code editor.
    However, in ADT editor `TYPE` is used, thus `STRUCTURE` needs to be
    converted to `TYPE`.
    
    Original source code:
    ```
        FUNCTION ztest_function.
        *"----------------------------------------------------------------------
        *"*"Local Interface:
        *"  IMPORTING
        *"     VALUE(IV_PARAM1) TYPE  STRING
        *"  EXPORTING
        *"     VALUE(EV_PARAM2) TYPE  STRING
        *"  TABLES
        *"     ET_PARAM3 STRUCTURE  BAPIRET2
        *"----------------------------------------------------------------------
        ...
        ENDFUNCTION.
    ```
    Formatted source code:
    ```
        FUNCTION ztest_function
        IMPORTING
        VALUE(IV_PARAM1) TYPE STRING
        EXPORTING
        VALUE(EV_PARAM2) TYPE STRING
        TABLES
        ET_PARAM3 TYPE  BAPIRET2
        .
        ...
        ENDFUNCTION.
    ```
    The indentation of the formatted source code is fixed upon write to the
    system.
    
    ---v2
    Fixed parameter type parsing. Added tests.

--- sap/cli/checkin.py
+++ sap/cli/checkin.py
@@ -415,13 +415,155 @@ def _check_fugr_source_files(repo_obj, functions, includes):
             raise sap.adt.errors.ExceptionCheckinFailure(f'No source file for include {include_name}')
 
 
+def _write_source_file(source_code, adt_object, corrnr=None):
+    with adt_object.open_editor(corrnr=corrnr) as editor:
+        editor.write(source_code)
+
+
 def _write_adt_object_source_file(path_prefix, adt_object, corrnr=None):
     """Write source file for ADT object"""
 
     adt_object_file_path = path_prefix + f'.{adt_object.name.lower()}' + '.abap'
     with open(adt_object_file_path, 'r', encoding='utf-8') as source:
-        with adt_object.open_editor(corrnr=corrnr) as editor:
-            editor.write(source.read())
+        _write_source_file(source.read(), adt_object, corrnr)
+
+
+def _get_parameters_block(source_lines):
+    """Get parameters block of function module
+
+    The block is delimited by '*"--' lines.
+    Example:
+    *"----------------------------------------------------------------------
+    *"*"Local Interface:
+    ...
+    *"----------------------------------------------------------------------
+    """
+
+    start_block = 0
+    end_block = 0
+    for i, line in enumerate(source_lines):
+        if line.startswith('*"--'):
+            start_block = i
+            break
+
+    for i, line in enumerate(source_lines[start_block + 1:]):
+        if line.startswith('*"--'):
+            end_block = i + start_block + 1
+            break
+
+    return start_block, end_block
+
+
+def _parse_function_parameters(parameters_block):
+    """Parse parameters of function module
+
+    From the parameters block:
+    ```
+        *"----------------------------------------------------------------------
+        *"*"Local Interface:
+        *"  IMPORTING
+        *"     VALUE(IV_PARAM1) TYPE  STRING
+        *"  EXPORTING
+        *"     VALUE(EV_PARAM2) TYPE  STRING
+        *"  TABLES
+        *"     ET_PARAM3 STRUCTURE  STRING
+        *"----------------------------------------------------------------------
+    ```
+    The parsed parameters are:
+    {
+        'IMPORTING': ['VALUE(IV_PARAM1) TYPE STRING'],
+        'EXPORTING': ['VALUE(EV_PARAM2) TYPE STRING'],
+        'CHANGING': [],
+        'TABLES': ['ET_PARAM3 TYPE  STRING'],
+        'EXCEPTIONS': []
+    }
+
+    Note the change from STRUCTURE to TYPE for TABLES parameters.
+    """
+
+    parameters = {
+        'IMPORTING': [],
+        'EXPORTING': [],
+        'CHANGING': [],
+        'TABLES': [],
+        'EXCEPTIONS': []
+    }
+    current_param = None
+    for line in parameters_block:
+        line = line.lstrip('*" ').rstrip()
+        if any(param == line for param in parameters):
+            current_param = line
+        elif current_param is not None:
+            param = line
+            parameters[current_param].append(param)
+
+    for i, table_param in enumerate(parameters['TABLES']):
+        var_name, abap_typing, *abap_type = table_param.split(' ')
+        if abap_typing == 'STRUCTURE':
+            parameters['TABLES'][i] = f'{var_name} TYPE {" ".join(abap_type)}'
+
+    return parameters
+
+
+def _format_function(source_code):
+    """Format parameters of function
+
+    Original source code:
+    ```
+        FUNCTION ztest_function.
+        *"----------------------------------------------------------------------
+        *"*"Local Interface:
+        *"  IMPORTING
+        *"     VALUE(IV_PARAM1) TYPE  STRING
+        *"  EXPORTING
+        *"     VALUE(EV_PARAM2) TYPE  STRING
+        *"----------------------------------------------------------------------
+        ...
+        ENDFUNCTION.
+    ```
+    Formatted source code:
+    ```
+        FUNCTION ztest_function
+        IMPORTING
+        VALUE(IV_PARAM1) TYPE STRING
+        EXPORTING
+        VALUE(EV_PARAM2) TYPE STRING
+        .
+        ...
+        ENDFUNCTION.
+    ```
+    """
+
+    source_lines = source_code.split('\n')
+    start_block, end_block = _get_parameters_block(source_lines)
+    if end_block == 0:
+        # Source code is in ADT format
+        return '\n'.join(source_lines)
+
+    source_lines[0] = source_lines[0].replace('.', '')  # Remove dot at the end of first line
+    parameters = _parse_function_parameters(source_lines[start_block:end_block])
+    adt_parameters_block = []
+    for param_type, params in parameters.items():
+        if not params:
+            continue
+
+        adt_parameters_block.append(f'{param_type}')
+        adt_parameters_block += params
+
+    adt_parameters_block.append('.')  # Add dot at the end of the parameters block
+    source_lines[start_block:end_block + 1] = adt_parameters_block
+    return '\n'.join(source_lines)
+
+
+def _write_function_source_code(path_prefix, adt_object, corrnr=None):
+    """Write source code for function. If function is in ababGit format, change it to ADT format"""
+
+    source_file_path = path_prefix + f'.{adt_object.name.lower()}' + '.abap'
+    with open(source_file_path, 'r', encoding='utf-8') as source:
+        source_code = source.read()
+
+    source_code = _format_function(source_code)
+    _write_source_file(source_code, adt_object, corrnr)
 
 
 def checkin_fugr(connection, repo_obj, corrnr=None):
@@ -476,7 +618,7 @@ def checkin_fugr(connection, repo_obj, corrnr=None):
             mod_log().info(err.message)
 
         sap.cli.core.printout('Writing Function Module:', function_module.name)
-        _write_adt_object_source_file(repo_obj.path[:-4], function_module, corrnr=corrnr)
+        _write_function_source_code(repo_obj.path[:-4], function_module, corrnr=corrnr)
 
     return abap_objs_inactive
 
