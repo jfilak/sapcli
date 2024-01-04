d28a0abc26124055b4a92a166b311be104bf2115
Jakub Filak <jakub@thefilaks.net>
2024-01-04 14:51:14 +0100

    platform: add a conversion from locale lang to SAP code
    To avoid the need to implement the parsing and checking everywhere
    where we may need it.
    
    We want to use current system locale for getting default language code
    where language is required.

--- test/unit/test_sap_platform_language.py
+++ test/unit/test_sap_platform_language.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import patch
 
 import sap.errors
 import sap.platform.language
@@ -28,6 +29,41 @@ class TestSAPPlatformLanguageCodes(unittest.TestCase):
             sap.platform.language.iso_code_to_sap_code('#')
 
         self.assertEqual(str(raised.exception), 'Not found ISO Code: #')
+
+
+class TestSAPPlatformLanguageLocale(unittest.TestCase):
+
+    def test_locale_lang_to_sap_code_ok(self):
+        with patch('sap.platform.language.getlocale', return_value=('en_US', 'UTF-8')):
+            sap_lang = sap.platform.language.locale_lang_sap_code()
+
+            self.assertEqual(sap_lang, 'E')
+
+    def test_locale_lang_to_sap_code_C(self):
+        """This is a special case for a short case because C is kinda special
+           as it is the POSIX portable locale and we may want to handle it
+           differently in feature.
+        """
+
+        with patch('sap.platform.language.getlocale', return_value=('C', 'UTF-8')):
+            with self.assertRaises(sap.errors.SAPCliError) as caught:
+                sap_lang = sap.platform.language.locale_lang_sap_code()
+
+        self.assertEqual(str(caught.exception), 'The current system locale language is not ISO 3166: C')
+
+    def test_locale_lang_to_sap_code_short(self):
+        with patch('sap.platform.language.getlocale', return_value=('e', 'UTF-8')):
+            with self.assertRaises(sap.errors.SAPCliError) as caught:
+                sap_lang = sap.platform.language.locale_lang_sap_code()
+
+        self.assertEqual(str(caught.exception), 'The current system locale language is not ISO 3166: e')
+
+    def test_locale_lang_to_sap_code_unknown(self):
+        with patch('sap.platform.language.getlocale', return_value=('WF', 'UTF-8')):
+            with self.assertRaises(sap.errors.SAPCliError) as caught:
+                sap_lang = sap.platform.language.locale_lang_sap_code()
+
+        self.assertEqual(str(caught.exception), 'The current system locale language cannot be converted to SAP language code: WF')
 
 
 if __name__ == '__main__':
