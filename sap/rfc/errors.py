fc5aa052d24ab328f9cef0fc5c4ec383a7258567
Jakub Filak <jakub.filak@sap.com>
2026-01-20 21:02:50 +0100

    wip: efective RFC errors

--- sap/rfc/errors.py
+++ sap/rfc/errors.py
@@ -6,10 +6,10 @@ class RFCConnectionError(SAPCliError):
     """Wrapper for RFC connection error"""
 
     def __init__(self, host, user, message):
-        self.message = f'[HOST:"{host}", USER:"{user}"] Error: {message}'
+        self.message = f'[HOST:"{host}", USER:"{user}"]: {message}'
 
     def __str__(self):
-        return f'RFC connection error: {self.message}'
+        return f'RFC error: {self.message}'
 
 
 class RFCLoginError(RFCConnectionError):
@@ -23,7 +23,36 @@ class RFCCommunicationError(RFCConnectionError):
     """Wrapper for RFC Communication error"""
 
     def __init__(self, host, user, exception):
-        msg = exception.message.split('\n')
-        msg = next(filter(lambda line: line.startswith('ERROR'), msg))
-        msg = ' '.join(msg.split()[1:])
-        super().__init__(host, user, msg)
+        # Convert:
+        # LOCATION    CPIC (TCP/IP) on local host with Unicode
+        # ERROR       partner
+        #             'very.long.dns.name.for.testing
+        #             .com:3301' not reached
+        # TIME        Tue Jan 20 17:44:07 2026
+        # RELEASE     753
+        # COMPONENT   NI (network interface)
+        # VERSION     40
+        # RC          -10
+        # MODULE      /bas/753_REL/src/base/ni/nixxi.cpp
+        # LINE        3458
+        # DETAIL      NiPConnect2: 10.10.10.7:3301
+        # SYSTEM CALL connect
+        # ERRNO       111
+        # ERRNO TEXT  Connection refused
+        # COUNTER     6
+        #
+        # To: partner 'very.long.dns.name.for.testing.com:3301' not reached
+
+        msg = ""
+        error_section = False
+        for line in exception.message.split('\n'):
+            if line.startswith('ERROR'):
+                error_section = True
+                msg = line.replace('ERROR', '').strip() + ' '
+            elif error_section:
+                if not line[0].isspace():
+                    error_section = False
+                else:
+                    msg += line.strip()
+
+        super().__init__(host, user, msg.strip())
