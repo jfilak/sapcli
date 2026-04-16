752560e31a031dc2bb18752289a5010ee7a0d265
Jakub Filak <jakub@thefilaks.net>
2026-04-16 18:37:02 +0000

    rest: port connection to sap/http/client
    To simplify and unify.
    
    Remove the 404 handling because it was copy pasta from the original
    module where it made sense because we had to support be able to use to
    login paths (old and new and ADT backed was not backward compatible).
    We have no such a request in REST modules yet. Even if we had it, the
    404 handler was buggy because it did not retry and left session
    uninitialized.
    
    Also added some URL path normalization to avoid unnecessary double
    slashing - currently none of the REST modules have such a problem but
    what if somebody used the API?

--- sap/rest/connection.py
+++ sap/rest/connection.py
@@ -2,22 +2,10 @@
 
 import json
 
-import socket
-from urllib3.connection import HTTPConnection
-import requests
-from requests.auth import HTTPBasicAuth
-
-from sap import get_logger, config_get
-from sap.http import (
-    HTTPRequestError,
-    UnexpectedResponseContent,
-    UnauthorizedError,
-    TimedOutRequestError,
-)
+import sap.http
+from sap import get_logger
+from sap.http import UnexpectedResponseContent
 from sap.rest.errors import GCTSConnectionError
-
-
-KEEPALIVE_CONFIGURED = False
 
 
 def mod_log():
@@ -26,28 +14,9 @@ def mod_log():
     return get_logger()
 
 
-def setup_keepalive():
-    """Make sure we send keepalive TCP packets"""
-
-    # pylint: disable=global-statement
-    global KEEPALIVE_CONFIGURED
-
-    if KEEPALIVE_CONFIGURED:
-        mod_log().debug("KeepAlive already configured")
-        return
-
-    KEEPALIVE_CONFIGURED = True
-
-    mod_log().debug("Updating urllib3.connection.HTTPConnection.default_socket_options with KeepAlive packets")
-
-    # Special thanks to: https://www.finbourne.com/blog/the-mysterious-hanging-client-tcp-keep-alives
-    # This may cause problems in Windows!
-    if "TCP_KEEPIDLE" in dir(socket):
-        # pylint: disable=no-member
-        HTTPConnection.default_socket_options = HTTPConnection.default_socket_options + [
-            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
-            (socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 60)
-        ]
+def _gcts_http_connection_error_handler(client, ex):
+    msg = str(ex)
+    raise GCTSConnectionError(client.host, client.port, client.ssl, msg) from ex
 
 
 # pylint: disable=too-many-instance-attributes
@@ -70,88 +39,39 @@ class Connection:
             - ssl_server_cert: optional path to a custom CA certificate file
         """
 
-        setup_keepalive()
+        sap.http.setup_keepalive()
 
-        if ssl:
-            protocol = 'https'
-            if port is None:
-                port = '443'
-        else:
-            protocol = 'http'
-            if port is None:
-                port = '80'
+        self._icf_path = icf_path
 
-        self._ssl_verify = verify
-        self._ssl_server_cert = ssl_server_cert
+        self._http_client = sap.http.HTTPClient(
+            ssl=ssl,
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            saml2=False,
+            client=client,
+            verify=verify,
+            ssl_server_cert=ssl_server_cert,
+            login_path=f'{icf_path}/{login_path.lstrip("/")}',
+            login_method='GET'
+        )
 
-        self._host = host
-        self._port = port
-        self._ssl = ssl
-        self._base_url = f'{protocol}://{host}:{port}/{icf_path}'
-        self._query_args = f'sap-client={client}&saml2=disabled'
+        self._http_client.set_connection_error_handler(_gcts_http_connection_error_handler)
 
-        self._user = user
-        self._auth = HTTPBasicAuth(user, password)
         self._session = None
-        self._login_path = login_path
-        self._timeout = config_get('http_timeout')
 
     @property
     def user(self):
         """Connected user"""
 
-        return self._user
+        return self._http_client.user
 
     def _build_url(self, uri_path):
-        """Creates complete URL from the path part
+        """Creates path from the URI part
         """
 
-        return f'{self._base_url}/{uri_path}?{self._query_args}'
-
-    def _handle_http_error(self, req, res):
-        """Raise the correct exception based on response content."""
-
-        if res.status_code == 401:
-            raise UnauthorizedError(req, res, self._user)
-
-        raise HTTPRequestError(req, res)
-
-    def _retrieve(self, session, method, url, params=None, headers=None, body=None):
-        """A helper method for easier testing."""
-
-        req = requests.Request(method.upper(), url, params=params, data=body, headers=headers)
-        req = session.prepare_request(req)
-
-        mod_log().info('Executing %s %s', method, url)
-
-        if body is not None:
-            mod_log().info('Body %s ', body)
-
-        try:
-            res = session.send(req, timeout=self._timeout)
-        except requests.exceptions.ConnectTimeout as ex:
-            raise TimedOutRequestError(req, self._timeout) from ex
-        except requests.exceptions.ReadTimeout as ex:
-            raise TimedOutRequestError(req, self._timeout) from ex
-        except requests.exceptions.ConnectionError as ex:
-            msg = str(ex)
-            raise GCTSConnectionError(self._host, self._port, self._ssl, msg) from ex
-
-        mod_log().debug('Response %s %s:\n++++\n%s\n++++', method, url, res.text)
-
-        return (req, res)
-
-    def _execute_with_session(self, session, method, url, params=None, headers=None, body=None):
-        """Executes the given URL using the given method in
-           the common HTTP session.
-        """
-
-        req, res = self._retrieve(session, method, url, params=params, headers=headers, body=body)
-
-        if res.status_code >= 400:
-            self._handle_http_error(req, res)
-
-        return res
+        return f'{self._icf_path}/{uri_path.lstrip("/")}'
 
     def _get_session(self):
         """Returns the working HTTP session.
@@ -160,31 +80,7 @@ class Connection:
         """
 
         if self._session is None:
-            self._session = requests.Session()
-            self._session.auth = self._auth
-            # requests.session.verify is either boolean or path to CA to use!
-            if self._ssl_server_cert:
-                self._session.verify = self._ssl_server_cert
-                mod_log().info('Using custom SSL Server cert path: %s', self._session.verify)
-            elif self._ssl_verify is False:
-                import urllib3
-                urllib3.disable_warnings()
-                mod_log().info('SSL Server cert will not be verified: SAP_SSL_VERIFY = no')
-                self._session.verify = False
-
-            login_headers = {'x-csrf-token': 'Fetch'}
-            csrf_token = None
-            url = self._build_url(self._login_path)
-
-            try:
-                response = self._execute_with_session(self._session, 'GET', url, headers=login_headers)
-            except HTTPRequestError as ex:
-                if ex.response.status_code != 404:
-                    raise ex
-
-            if 'x-csrf-token' in response.headers:
-                csrf_token = response.headers['x-csrf-token']
-                self._session.headers.update({'x-csrf-token': csrf_token})
+            self._session, _ = self._http_client.build_session()
 
         return self._session
 
@@ -212,7 +108,7 @@ class Connection:
         if not headers:
             headers = None
 
-        resp = self._execute_with_session(session, method, url, params=params, headers=headers, body=body)
+        resp = self._http_client.execute_with_session(session, method, url, params=params, headers=headers, body=body)
 
         if accept:
             resp_content_type = resp.headers['Content-Type']
--- test/unit/test_sap_rest_connection.py
+++ test/unit/test_sap_rest_connection.py
@@ -7,13 +7,14 @@ from requests.exceptions import ConnectionError, ReadTimeout
 from unittest.mock import Mock, patch
 
 from sap.rest.connection import Connection
-from sap.rest.errors import UnauthorizedError, GCTSConnectionError, TimedOutRequestError
+from sap.http import HTTPRequestError
+from sap.rest.errors import UnauthorizedError, GCTSConnectionError, TimedOutRequestError, UnexpectedResponseContent
 
 
-def stub_retrieve(response, session, method, url, params=None, headers=None, body=None):
+def stub_retrieve(response, session, method, path, params=None, headers=None, body=None):
     req = Mock()
     req.method = method
-    req.url = url
+    req.url = path
     req.params = params
     req.headers = headers
     req.body = body
@@ -24,94 +25,197 @@ def stub_retrieve(response, session, method, url, params=None, headers=None, bod
 class TestConnectionExecute(unittest.TestCase):
 
     def setUp(self):
-        icf_path = '/foo'
+        self.icf_path = '/foo'
         login_path = '/bar'
         host = 'books.fr'
         client = '69'
-        user = 'Arsan'
+        self.user = 'Arsan'
         password = 'Emmanuelle'
 
-        self.conn = Connection(icf_path, login_path, host, client, user, password)
+        self.conn = Connection(self.icf_path, login_path, host, client, self.user, password)
 
-    @patch('sap.rest.connection.Connection._retrieve')
-    def test_unauthorized_error(self, fake_retrieve):
-        icf_path = '/foo'
-        login_path = '/bar'
-        host = 'books.fr'
-        client = '69'
-        user = 'Arsan'
-        password = 'Emmanuelle'
-        method = 'GET'
-        url = '/all'
-
-        conn = Connection(icf_path, login_path, host, client, user, password)
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.HTTPClient.retrieve')
+    def test_unauthorized_error(self, fake_retrieve, mock_get_session):
+        mock_get_session.return_value = Mock()
 
         res = Mock()
         res.status_code = 401
         fake_retrieve.side_effect = partial(stub_retrieve, res)
 
+        method = 'GET'
+        uri_path = 'all'
+
         with self.assertRaises(UnauthorizedError) as caught:
-            conn._execute_with_session(conn._session, method, url)
+            self.conn.execute(method, uri_path)
 
-        self.assertEqual(str(caught.exception), f'Authorization for the user "{user}" has failed: {method} {url}')
+        expected_path = f'{self.icf_path}/{uri_path}'
+        self.assertEqual(str(caught.exception),
+                         f'Authorization for the user "{self.user}" has failed: {method} {expected_path}')
 
-    @patch('sap.rest.connection.requests.Request')
-    def test_protocol_error(self, _):
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.requests.Request')
+    def test_protocol_error(self, _, mock_get_session):
         session = Mock()
         session.send.side_effect = ConnectionError('Remote end closed connection without response')
-
-        method = 'GET'
-        url = '/all'
+        mock_get_session.return_value = session
 
         with self.assertRaises(GCTSConnectionError) as cm:
-            self.conn._retrieve(session, method, url)
+            self.conn.execute('GET', 'all')
 
         self.assertEqual(str(cm.exception),
-                         f'GCTS connection error: [HOST:"{self.conn._host}", PORT:"443", '
+                         f'GCTS connection error: [HOST:"{self.conn._http_client.host}", PORT:"443", '
                          'SSL:"True"] Error: Remote end closed connection without response')
 
-    @patch('sap.rest.connection.requests.Request')
-    def test_dns_error(self, _):
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.requests.Request')
+    def test_dns_error(self, _, mock_get_session):
         session = Mock()
         session.send.side_effect = ConnectionError('[Errno -5] Dummy name resolution error')
-
-        method = 'GET'
-        url = '/all'
+        mock_get_session.return_value = session
 
         with self.assertRaises(GCTSConnectionError) as cm:
-            self.conn._retrieve(session, method, url)
+            self.conn.execute('GET', 'all')
 
         self.assertEqual(str(cm.exception),
-                         f'GCTS connection error: [HOST:"{self.conn._host}", PORT:"443", '
+                         f'GCTS connection error: [HOST:"{self.conn._http_client.host}", PORT:"443", '
                          'SSL:"True"] Error: Name resolution error. Check the HOST configuration.')
 
-    @patch('sap.rest.connection.requests.Request')
-    def test_connection_error(self, _):
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.requests.Request')
+    def test_connection_error(self, _, mock_get_session):
         session = Mock()
         session.send.side_effect = ConnectionError('[Errno 111] Dummy connection error')
-
-        method = 'GET'
-        url = '/all'
+        mock_get_session.return_value = session
 
         with self.assertRaises(GCTSConnectionError) as cm:
-            self.conn._retrieve(session, method, url)
+            self.conn.execute('GET', 'all')
 
         self.assertEqual(str(cm.exception),
-                         f'GCTS connection error: [HOST:"{self.conn._host}", PORT:"443", '
+                         f'GCTS connection error: [HOST:"{self.conn._http_client.host}", PORT:"443", '
                          'SSL:"True"] Error: Cannot connect to the system. Check the HOST and PORT configuration.')
 
-    @patch('sap.rest.connection.requests.Request')
-    def test_read_timeout(self, _):
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.requests.Request')
+    def test_read_timeout(self, _, mock_get_session):
         session = Mock()
         session.send.side_effect = ReadTimeout('HTTPSConnectionPool read timed out')
-
-        method = 'GET'
-        url = '/all'
+        mock_get_session.return_value = session
 
         with self.assertRaises(TimedOutRequestError) as cm:
-            self.conn._retrieve(session, method, url)
+            self.conn.execute('GET', 'all')
 
         self.assertIn('took more than', str(cm.exception))
+
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.HTTPClient.execute_with_session')
+    def test_execute_no_optional_args(self, mock_exec, mock_get_session):
+        """No accept, content_type, or headers → headers=None passed to execute_with_session."""
+        mock_get_session.return_value = Mock()
+        mock_exec.return_value = Mock()
+
+        self.conn.execute('GET', 'all')
+
+        self.assertIsNone(mock_exec.call_args.kwargs['headers'])
+
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.HTTPClient.execute_with_session')
+    def test_execute_accept_string(self, mock_exec, mock_get_session):
+        """accept as string → Accept header set and response Content-Type validated."""
+        mock_get_session.return_value = Mock()
+        response = Mock()
+        response.headers = {'Content-Type': 'application/json'}
+        mock_exec.return_value = response
+
+        self.conn.execute('GET', 'all', accept='application/json')
+
+        self.assertEqual(mock_exec.call_args.kwargs['headers'], {'Accept': 'application/json'})
+
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.HTTPClient.execute_with_session')
+    def test_execute_accept_list(self, mock_exec, mock_get_session):
+        """accept as list → Accept header is comma-joined and response Content-Type validated."""
+        mock_get_session.return_value = Mock()
+        response = Mock()
+        response.headers = {'Content-Type': 'text/xml'}
+        mock_exec.return_value = response
+
+        self.conn.execute('GET', 'all', accept=['application/json', 'text/xml'])
+
+        self.assertEqual(mock_exec.call_args.kwargs['headers'], {'Accept': 'application/json, text/xml'})
+
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.HTTPClient.execute_with_session')
+    def test_execute_content_type(self, mock_exec, mock_get_session):
+        """content_type set → Content-Type header set; no response Content-Type validation."""
+        mock_get_session.return_value = Mock()
+        mock_exec.return_value = Mock()
+
+        self.conn.execute('POST', 'all', content_type='application/json')
+
+        self.assertEqual(mock_exec.call_args.kwargs['headers'], {'Content-Type': 'application/json'})
+
+    @patch('sap.http.client.HTTPClient.build_session')
+    def test_get_session_propagates_404(self, mock_build_session):
+        req = Mock()
+        res = Mock()
+        res.status_code = 404
+        mock_build_session.side_effect = HTTPRequestError(req, res)
+
+        with self.assertRaises(HTTPRequestError):
+            self.conn._get_session()
+
+    @patch('sap.rest.connection.Connection._get_session')
+    @patch('sap.http.client.HTTPClient.execute_with_session')
+    def test_execute_accept_content_type_mismatch(self, mock_exec, mock_get_session):
+        """Response Content-Type not in accept → UnexpectedResponseContent raised."""
+        mock_get_session.return_value = Mock()
+        response = Mock()
+        response.headers = {'Content-Type': 'text/html'}
+        mock_exec.return_value = response
+
+        with self.assertRaises(UnexpectedResponseContent):
+            self.conn.execute('GET', 'all', accept='application/json')
+
+
+class TestConnectionPathJoining(unittest.TestCase):
+    """Verify that path joining in __init__ and _build_url never produces double slashes."""
+
+    # --- __init__: login_path = f'{icf_path}/{login_path}' ---
+
+    def test_login_path_no_leading_slashes(self):
+        conn = Connection('sap/bc/cts', 'system', 'host', '100', 'user', 'pass')
+        self.assertEqual(conn._http_client.login_path, 'sap/bc/cts/system')
+
+    def test_login_path_login_has_leading_slash(self):
+        conn = Connection('sap/bc/cts', '/system', 'host', '100', 'user', 'pass')
+        self.assertEqual(conn._http_client.login_path, 'sap/bc/cts/system')
+
+    def test_login_path_icf_has_leading_slash(self):
+        conn = Connection('/sap/bc/cts', 'system', 'host', '100', 'user', 'pass')
+        self.assertEqual(conn._http_client.login_path, '/sap/bc/cts/system')
+
+    def test_login_path_both_have_leading_slash(self):
+        conn = Connection('/sap/bc/cts', '/system', 'host', '100', 'user', 'pass')
+        self.assertEqual(conn._http_client.login_path, '/sap/bc/cts/system')
+
+    # --- _build_url: f'{self._icf_path}/{uri_path}' ---
+
+    def test_build_url_no_leading_slashes(self):
+        conn = Connection('sap/bc/cts', 'system', 'host', '100', 'user', 'pass')
+        self.assertEqual(conn._build_url('repository'), 'sap/bc/cts/repository')
+
+    def test_build_url_uri_has_leading_slash(self):
+        conn = Connection('sap/bc/cts', 'system', 'host', '100', 'user', 'pass')
+        self.assertEqual(conn._build_url('/repository'), 'sap/bc/cts/repository')
+
+    def test_build_url_icf_has_leading_slash(self):
+        conn = Connection('/sap/bc/cts', 'system', 'host', '100', 'user', 'pass')
+        self.assertEqual(conn._build_url('repository'), '/sap/bc/cts/repository')
+
+    def test_build_url_both_have_leading_slash(self):
+        conn = Connection('/sap/bc/cts', 'system', 'host', '100', 'user', 'pass')
+        self.assertEqual(conn._build_url('/repository'), '/sap/bc/cts/repository')
 
 
 class TestConnectionSSLServerCert(unittest.TestCase):
@@ -119,14 +223,14 @@ class TestConnectionSSLServerCert(unittest.TestCase):
 
     def test_ssl_server_cert_default_none(self):
         conn = Connection('/foo', '/bar', 'host', '100', 'user', 'pass')
-        self.assertIsNone(conn._ssl_server_cert)
+        self.assertIsNone(conn._http_client.ssl_server_cert)
 
     def test_ssl_server_cert_stored(self):
         conn = Connection('/foo', '/bar', 'host', '100', 'user', 'pass',
                           ssl_server_cert='/path/to/ca.pem')
-        self.assertEqual(conn._ssl_server_cert, '/path/to/ca.pem')
+        self.assertEqual(conn._http_client.ssl_server_cert, '/path/to/ca.pem')
 
-    @patch('sap.rest.connection.Connection._execute_with_session')
+    @patch('sap.http.client.HTTPClient.execute_with_session')
     def test_ssl_server_cert_sets_session_verify(self, mock_exec):
         """When ssl_server_cert is set, session.verify is the cert path."""
         mock_response = Mock()
@@ -139,7 +243,7 @@ class TestConnectionSSLServerCert(unittest.TestCase):
         session = conn._get_session()
         self.assertEqual(session.verify, '/path/to/ca.pem')
 
-    @patch('sap.rest.connection.Connection._execute_with_session')
+    @patch('sap.http.client.HTTPClient.execute_with_session')
     def test_ssl_server_cert_takes_precedence_over_verify_false(self, mock_exec):
         """ssl_server_cert takes precedence over verify=False."""
         mock_response = Mock()
