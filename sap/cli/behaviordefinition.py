8561ea58d28e52871b797fa52653840e2ee7d4c6
Jakub Filak <jakub.filak@sap.com>
2026-05-11 17:29:06 +0200

    clie: enhance CLI create command to support behavior extensions
    Add --base-bdef and --interface-bdef optional arguments to the
    'sapcli bdef create' command. When --base-bdef is provided, the
    command creates a behavior extension instead of a regular behavior
    definition by setting the adtcore:adtTemplate in the request body.
    
    When --interface-bdef is provided, the command validates that the
    interface is assigned to the base behavior definition (or its
    extensions) by calling list_interfaces. Validation uses case-
    insensitive comparison as specified.
    
    Decision: add new command 'extend' to make it super clear how to
    create new extension to behavior definition.

--- sap/cli/behaviordefinition.py
+++ sap/cli/behaviordefinition.py
@@ -4,7 +4,9 @@ import sap.adt
 import sap.adt.wb
 import sap.cli.core
 import sap.cli.object
+from sap.errors import SAPCliError
 from sap.adt.behaviordefinition import BehaviorDefinition
+from sap.adt.common_types import ADTTemplate, ADTTemplateProperty
 
 
 class CommandGroup(sap.cli.object.CommandGroupObjectMaster):
@@ -23,6 +25,53 @@ class CommandGroup(sap.cli.object.CommandGroupObjectMaster):
             package = args.package
 
         return sap.adt.BehaviorDefinition(connection, name.upper(), package=package, metadata=metadata)
+
+    def define(self):
+        commands = super().define()
+        if commands is not None:
+            self.define_extend(commands)
+        return commands
+
+    def define_extend(self, commands):
+        """Declares the Extend command with its parameters."""
+
+        extend_cmd = commands.add_command(self.extend_object, name='extend')
+        extend_cmd.append_argument('name')
+        extend_cmd.append_argument('description')
+        extend_cmd.append_argument('package')
+        extend_cmd.append_argument('base_bdef', metavar='base-bdef')
+        extend_cmd.append_argument('--interface-bdef', default=None,
+                                   help='BO Interface to assign the extension to')
+        extend_cmd.declare_corrnr()
+
+        return extend_cmd
+
+    def extend_object(self, connection, args):
+        """Creates a new behavior extension."""
+
+        base_bdef = args.base_bdef.upper()
+        interface_bdef = getattr(args, 'interface_bdef', None)
+
+        if interface_bdef:
+            interface_bdef = interface_bdef.upper()
+            result = BehaviorDefinition.list_interfaces(connection, base_bdef)
+            interface_names = [item.name.lower() for item in result.items]
+            if interface_bdef.lower() not in interface_names:
+                raise SAPCliError(
+                    f'Interface {interface_bdef} is not assigned to behavior definition '
+                    f'{base_bdef} or any of its extensions'
+                )
+
+        metadata = self.build_new_metadata(connection, args)
+        obj = self.build_new_object(connection, args, metadata)
+        obj.description = args.description
+
+        obj.template = ADTTemplate([
+            ADTTemplateProperty('base_bdef', base_bdef),
+            ADTTemplateProperty('interface_bdef', interface_bdef),
+        ])
+
+        obj.create(corrnr=args.corrnr)
 
 
 @CommandGroup.argument('name')
