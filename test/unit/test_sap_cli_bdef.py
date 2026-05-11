23378449c9d9f0358f38317738843322d15d7800
Jakub Filak <jakub.filak@sap.com>
2026-05-11 17:29:06 +0200

    clie: enhance CLI create command to support behavior extensions
    Add --base-bdef and --interface-bdef optional arguments to the
    'sapcli bdef create' command. When --base-bdef is provided, the
    command creates a behavior extension instead of a regular behavior
    definition by setting the adtcore:adtTemplate in the request body.
    
    When --interface-bdef is provided, the command validates that the
    interface is assigned to the base behavior definition (or its
    extensions) by calling list_interfaces. Validation uses case-
    insensitive comparison as specified.
    
    Decision: add new command 'extend' to make it super clear how to
    create new extension to behavior definition.

--- test/unit/test_sap_cli_bdef.py
+++ test/unit/test_sap_cli_bdef.py
@@ -7,6 +7,7 @@ from io import StringIO
 
 import sap.cli.interface
 import sap.cli.behaviordefinition
+from sap.errors import SAPCliError
 
 from infra import generate_parse_args
 from mock import patch_get_print_console_with_buffer
@@ -113,6 +114,163 @@ class TestBDEFListInterfaces(unittest.TestCase):
             args.execute(fake_conn, args)
 
         self.assertEqual(fake_console.capout, '')
+
+
+class TestBDEFCreate(unittest.TestCase):
+
+    @patch('sap.adt.BehaviorDefinition')
+    def test_cli_bdef_create_regular(self, fake_bdef):
+        """Regular create without extension params should work as before"""
+        fake_conn = Mock()
+        fake_conn.user = 'DEVELOPER'
+        fake_instance = Mock()
+        fake_instance.template = None
+        fake_bdef.return_value = fake_instance
+
+        args = parse_args('create', 'ZMYBDEF', 'My description', 'MYPACKAGE')
+        args.execute(fake_conn, args)
+
+        fake_bdef.assert_called_once()
+        fake_instance.create.assert_called_once_with(corrnr=None)
+        self.assertIsNone(fake_instance.template)
+
+
+class TestBDEFExtend(unittest.TestCase):
+
+    @patch('sap.adt.BehaviorDefinition')
+    def test_cli_bdef_extend_without_interface(self, fake_bdef):
+        """Extend with base-bdef but no --interface-bdef"""
+        fake_conn = Mock()
+        fake_conn.user = 'DEVELOPER'
+        fake_instance = Mock()
+        fake_instance.template = None
+        fake_bdef.return_value = fake_instance
+
+        args = parse_args('extend', 'R_PRODUCTTP_EXT', 'My extension', 'MYPACKAGE',
+                          'R_PRODUCTTP')
+        args.execute(fake_conn, args)
+
+        fake_instance.create.assert_called_once_with(corrnr=None)
+        self.assertIsNotNone(fake_instance.template)
+        template = fake_instance.template
+        self.assertEqual(len(template.properties), 2)
+        self.assertEqual(template.properties[0].key, 'base_bdef')
+        self.assertEqual(template.properties[0].value, 'R_PRODUCTTP')
+        self.assertEqual(template.properties[1].key, 'interface_bdef')
+        self.assertIsNone(template.properties[1].value)
+
+    @patch('sap.adt.behaviordefinition.BehaviorDefinition.list_interfaces')
+    @patch('sap.adt.BehaviorDefinition')
+    def test_cli_bdef_extend_with_valid_interface(self, fake_bdef, fake_list):
+        """Extend with valid --interface-bdef"""
+        fake_conn = Mock()
+        fake_conn.user = 'DEVELOPER'
+        fake_instance = Mock()
+        fake_instance.template = None
+        fake_bdef.return_value = fake_instance
+
+        fake_item = Mock()
+        fake_item.name = 'I_PRODUCTTP_2'
+        fake_result = Mock()
+        fake_result.items = [fake_item]
+        fake_list.return_value = fake_result
+
+        args = parse_args('extend', 'R_PRODUCTTP_EXT', 'My extension', 'MYPACKAGE',
+                          'R_PRODUCTTP', '--interface-bdef', 'I_PRODUCTTP_2')
+        args.execute(fake_conn, args)
+
+        fake_list.assert_called_once_with(fake_conn, 'R_PRODUCTTP')
+        fake_instance.create.assert_called_once_with(corrnr=None)
+        template = fake_instance.template
+        self.assertEqual(template.properties[1].key, 'interface_bdef')
+        self.assertEqual(template.properties[1].value, 'I_PRODUCTTP_2')
+
+    @patch('sap.adt.behaviordefinition.BehaviorDefinition.list_interfaces')
+    @patch('sap.adt.BehaviorDefinition')
+    def test_cli_bdef_extend_with_valid_interface_case_insensitive(self, fake_bdef, fake_list):
+        """Interface validation is case-insensitive"""
+        fake_conn = Mock()
+        fake_conn.user = 'DEVELOPER'
+        fake_instance = Mock()
+        fake_instance.template = None
+        fake_bdef.return_value = fake_instance
+
+        fake_item = Mock()
+        fake_item.name = 'I_PRODUCTTP_2'
+        fake_result = Mock()
+        fake_result.items = [fake_item]
+        fake_list.return_value = fake_result
+
+        args = parse_args('extend', 'R_PRODUCTTP_EXT', 'My extension', 'MYPACKAGE',
+                          'R_PRODUCTTP', '--interface-bdef', 'i_producttp_2')
+        args.execute(fake_conn, args)
+
+        fake_instance.create.assert_called_once_with(corrnr=None)
+
+    @patch('sap.adt.BehaviorDefinition')
+    def test_cli_bdef_extend_uppercases_base_bdef(self, fake_bdef):
+        """Extend uppercases base_bdef input"""
+        fake_conn = Mock()
+        fake_conn.user = 'DEVELOPER'
+        fake_instance = Mock()
+        fake_instance.template = None
+        fake_bdef.return_value = fake_instance
+
+        args = parse_args('extend', 'R_PRODUCTTP_EXT', 'My extension', 'MYPACKAGE',
+                          'r_producttp')
+        args.execute(fake_conn, args)
+
+        template = fake_instance.template
+        self.assertEqual(template.properties[0].value, 'R_PRODUCTTP')
+
+    @patch('sap.adt.behaviordefinition.BehaviorDefinition.list_interfaces')
+    @patch('sap.adt.BehaviorDefinition')
+    def test_cli_bdef_extend_uppercases_interface_bdef(self, fake_bdef, fake_list):
+        """Extend uppercases interface_bdef input"""
+        fake_conn = Mock()
+        fake_conn.user = 'DEVELOPER'
+        fake_instance = Mock()
+        fake_instance.template = None
+        fake_bdef.return_value = fake_instance
+
+        fake_item = Mock()
+        fake_item.name = 'I_PRODUCTTP_2'
+        fake_result = Mock()
+        fake_result.items = [fake_item]
+        fake_list.return_value = fake_result
+
+        args = parse_args('extend', 'R_PRODUCTTP_EXT', 'My extension', 'MYPACKAGE',
+                          'r_producttp', '--interface-bdef', 'i_producttp_2')
+        args.execute(fake_conn, args)
+
+        template = fake_instance.template
+        self.assertEqual(template.properties[0].value, 'R_PRODUCTTP')
+        self.assertEqual(template.properties[1].value, 'I_PRODUCTTP_2')
+
+    @patch('sap.adt.behaviordefinition.BehaviorDefinition.list_interfaces')
+    @patch('sap.adt.BehaviorDefinition')
+    def test_cli_bdef_extend_with_invalid_interface(self, fake_bdef, fake_list):
+        """Extend with --interface-bdef that doesn't exist should fail"""
+        fake_conn = Mock()
+        fake_conn.user = 'DEVELOPER'
+        fake_instance = Mock()
+        fake_instance.template = None
+        fake_bdef.return_value = fake_instance
+
+        fake_item = Mock()
+        fake_item.name = 'I_PRODUCTTP_2'
+        fake_result = Mock()
+        fake_result.items = [fake_item]
+        fake_list.return_value = fake_result
+
+        args = parse_args('extend', 'R_PRODUCTTP_EXT', 'My extension', 'MYPACKAGE',
+                          'R_PRODUCTTP', '--interface-bdef', 'I_NONEXISTENT')
+
+        with self.assertRaises(SAPCliError) as cm:
+            args.execute(fake_conn, args)
+
+        self.assertIn('I_NONEXISTENT', str(cm.exception))
+        fake_instance.create.assert_not_called()
 
 
 if __name__ == '__main__':
